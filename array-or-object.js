239a820650f6d3c50b92b92dee209f2446458301
Kyle Mitchell <kyle@kemitchell.com>
2015-09-23 23:08:17 -0700

    Improve code

--- array-or-object.js
+++ array-or-object.js
@@ -4,13 +4,9 @@ process.stdin
   .pipe(split2())
   .on('data', function(buffer) {
     var object = JSON.parse(buffer)
-    if (!object.hasOwnProperty('name')) {
-      return }
+    if (!object.hasOwnProperty('name')) { return }
     var license = object.license
-    var type = (
-      Array.isArray(license) ?
-        'array' :
-        ( typeof license === 'object' ?
-            'object' : false ) )
-    if (type) {
-      process.stdout.write(object.name + '\t' + type + '\n') } })
+    var type = false
+    if (Array.isArray(license)) { type = 'array' }
+    else if (typeof license === 'object') { type = 'object' }
+    if (type) { process.stdout.write(object.name + '\t' + type + '\n') } })
